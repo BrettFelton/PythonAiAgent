72e9e15d6ff7efcc57586d77845e7928a5715443
BrettFelton <bmfelton@live.com>
2026-03-09 12:16:53 +0200

    Added token usage for both prompt and response

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from google import genai
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
-    if api_key is None:
-        raise RuntimeError("Enviroment variable not loaded.")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model = "gemini-2.5-flash",
         contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
